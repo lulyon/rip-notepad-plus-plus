f0c90552c98a4ea19b3fbcc46be5c0afc3f24ae2
luliang <luliang@zhihu.com>
2026-06-15 17:17:37 +0800

    fix: add React import for Fragment

--- src/components/Panels/AiPanel.tsx
+++ src/components/Panels/AiPanel.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useAiStore } from "../../stores/aiStore";
 import { useEditorStore } from "../../stores/editorStore";
